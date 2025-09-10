be28686718cfc5b80c1b32c26e81d78ad9a16e42
syahputrawork98-sketch <syah.putrawork98@gmail.com>
2025-09-10 16:09:33 +0700

    perubahan sesi 2 dashhboard

--- client/src/components/dasboard/CD_Dasboard.jsx
+++ client/src/components/dasboard/CD_Dasboard.jsx
@@ -1,11 +1,10 @@
 import React from 'react'
-import CDT_Proyek from '../tabel/CDT_Proyek';
+import CDT_Proyek from './tabel/CDT_Proyek';
 import { IoLogOutOutline } from "react-icons/io5";
-
 
 const CD_Dasboard = () => {
   return (
-    <div className="container min-h-screen">
+    <div className="m-t-2">
       <div className="border-b shadow mb-4 mt-2 pb-4 border-stone-300">
         {/* Header */}
         <div className="flex justify-between items-center px-4 py-2">
@@ -21,7 +20,7 @@ const CD_Dasboard = () => {
             </span>
           </div>
           <button className="btn btn-outline btn-secondary">
-            <IoLogOutOutline />
+           <IoLogOutOutline />
             Logout
           </button>
         </div>
